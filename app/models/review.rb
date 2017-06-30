959c0f3b1a88e77d0069f3c5714435eb63b48258
Nick Powell <nickpowells@gmail.com>
2017-06-30 14:31:46 -0700

    add scopes to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,9 @@ class Review < ActiveRecord::Base
   validates :name, :presence => true
   validates :comment, :presence => true
   validates :rating, :presence => true
+
+
+  scope :most_recent, -> { order(created_at: :asc) }
+  scope :oldest, -> { order(created_at: :desc) }
+  scope :best_review, -> { order(rating: :asc).limit(2) }
 end
