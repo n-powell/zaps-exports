94252dbbc41c5b6ac036e97daf62bc1b50a03545
Nick Powell <nickpowells@gmail.com>
2017-06-30 13:32:40 -0700

    add product create integration spec

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development, :test do
   gem 'launchy'
   gem 'pry'
   gem 'shoulda-matchers'
+  gem 'capybara'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
 
