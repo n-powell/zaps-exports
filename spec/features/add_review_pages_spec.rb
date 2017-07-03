8192a9ae525f7b8bf429f38d817aac66e644bc63
Nick Powell <nickpowells@gmail.com>
2017-07-03 16:19:39 -0700

    fix review spec

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -8,19 +8,19 @@ describe "the add a review test" do
     visit products_path
     click_on 'Add New Product'
     fill_in 'Name', :with => 'Zebraz'
-    fill_in 'Style', :with => 'beverage'
+    fill_in 'Style', :with => 'Beverage'
     fill_in 'Price', :with => '2.50'
     fill_in 'From', :with => 'USA'
     click_on 'Create Product'
-    within(:css, "div.full-list") do
+    within(:css, "div.most-recent") do
       click_on 'Zebraz'
     end
     click_on 'Add a review'
-    click_on 'Create Review'
     fill_in 'Name', :with => 'John'
     fill_in 'Comment', :with => 'This is a very fantastic product. I would absulutely recommend this to anyone who asks.'
     fill_in 'Rating', :with => '5'
-    expect(page).to have_content 'successfully'
+    click_on 'Create Review'
+    expect(page).to have_content "Review successfully added!"
   end
 
 
