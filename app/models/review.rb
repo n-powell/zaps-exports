94bfabf2e874ce1678cfc46e40e42b7eeef3c1aa
Nick Powell <nickpowells@gmail.com>
2017-07-02 22:28:42 -0700

    add first review scope

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,6 +9,6 @@ class Review < ActiveRecord::Base
 
   scope :most_recent, -> { order(created_at: :desc) }
   scope :first_review, -> { order(created_at: :asc).limit(1) }
-  scope :best_review, -> { order(rating: :asc).limit(2) }
-  scope :worst_reviews, -> { order(rating: :desc).limit(2) }
+  scope :best_reviews, -> { order(rating: :desc).limit(2) }
+  scope :worst_reviews, -> { order(rating: :asc).limit(2) }
 end
