92179bd4d5c63603c36bcbdafbf7be94a68bfd53
Nick Powell <nickpowells@gmail.com>
2017-07-03 00:07:42 -0700

    add food and bev sort

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,12 +7,14 @@ class Product < ActiveRecord::Base
   scope :most_recent, -> { order(created_at: :asc).limit(5) }
   scope :highest_ppu, -> { order(price: :desc).limit(10) }
   scope :lowest_ppu, -> { order(price: :asc).limit(10) }
+  scope :is_food, -> { where(style: "Food") }
+  scope :is_bev, -> { where(style: "Beverage") }
 
   scope :most_reviews, -> {(
   select("products.id, products.name, products.style, count(reviews.id) as reviews_count")
   .joins(:reviews)
   .group("products.id")
   .order("reviews_count DESC")
-  .limit(3)
+  .limit(8)
   )}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,19 @@
 Product.destroy_all
 Review.destroy_all
 
-50.times do |index|
+48.times do |index|
   Product.create!(name: Faker::App.name,
                         style: "Food",
                         price: Faker::Number.decimal(2))
 end
 
-300.times do |index|
+29.times do |index|
+  Product.create!(name: Faker::App.name,
+                        style: "Beverage",
+                        price: Faker::Number.decimal(2))
+end
+
+314.times do |index|
   Review.create!(name: Faker::App.name,
                         comment: Faker::Lorem.sentence(10, true, 4),
                         rating: Faker::Number.between(1,5),
